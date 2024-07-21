0faa383b62102a344e82add98ebd1de20708b2f7
Yash Chavan <yashchavan4628@gmail.com>
2024-07-21 22:15:18 +0530

    upload image functionality temporary

--- client/src/pages/Auth/Profile.jsx
+++ client/src/pages/Auth/Profile.jsx
@@ -54,7 +54,7 @@ function Profile() {
                     onChange={(e) => setImage(e.target.files[0])}
 
                 />
-                <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24  self-center  cursor-pointer 
+                <img src={formData.profilePicture || currentUser.profilePicture} alt='profile' className='h-24 w-24  self-center  cursor-pointer 
             rounded-full
             object-cover mt-2
             ' onClick={() => fileRef.current.click()} />
