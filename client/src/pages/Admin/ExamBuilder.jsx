8736ef796be81deff076cc768fe189a17b553417
Yash Chavan <yashchavan4628@gmail.com>
2024-09-05 18:57:22 +0530

    everything is working perfectly fine. The exam questions making system is working perfectly fine now just we have to connect it to backend.

--- client/src/pages/Admin/ExamBuilder.jsx
+++ client/src/pages/Admin/ExamBuilder.jsx
@@ -69,12 +69,12 @@ function ExamBuilder() {
 
   return (
     <div>
-        <section className='flex flex-row justify-between m-2'>
-            <h2 className='font-bold text-2xl'>Prepare Exam Paper</h2>
-                <button className='button' 
-                    onClick={addQuestion}>Add Question
-                </button>
-            {/* <div className='flex justify-around w-1/2'>
+      <section className='flex flex-row justify-between m-2'>
+        <h2 className='font-bold text-2xl'>Prepare Exam Paper</h2>
+        <button className='button'
+          onClick={addQuestion}>Add Question
+        </button>
+        {/* <div className='flex justify-around w-1/2'>
                 <button className='button' 
                     onClick={addQuestion}>Add Question
                 </button>
@@ -85,63 +85,62 @@ function ExamBuilder() {
                     Save Paper
                 </button>
             </div> */}
-        </section>
+      </section>
 
       <section className='flex flex-col flex-wrap w-[90%] mx-auto'>
         {questions.map((question, index) => (
           <section key={question.id} className='my-4'>
-          <div className='flex flex-row justify-between text-center my-2'>
-                <h3 className='font-bold my-auto'>Question {index + 1}</h3>
-                {/* Question Text */}
-                <input
-                    type="text"
-                    className='w-[75%] m-1 p-2 bg-sky-500/10 rounded-xl border-4'
-                    placeholder="Enter question text"
-                    value={question.text}
-                    onChange={(e) => updateQuestionText(question.id, e.target.value)}
-                    />
-                {/* Select Question Type */}
-                <label className='my-auto border-2 p-1 rounded-full'>
+            <div className='flex flex-row justify-between text-center my-2'>
+              <h3 className='font-bold my-auto'>Question {index + 1}</h3>
+              {/* Question Text */}
+              <input
+                type="text"
+                className='w-[75%] m-1 p-2 bg-sky-500/10 rounded-xl border-4'
+                placeholder="Enter question text"
+                value={question.text}
+                onChange={(e) => updateQuestionText(question.id, e.target.value)}
+              />
+              {/* Select Question Type */}
+              <label className='my-auto border-2 p-1 rounded-full'>
                 Question Type:
                 <select
-                    value={question.type}
-                    onChange={(e) => updateQuestionType(question.id, e.target.value)}
-                    className='my-auto mx-auto font-bold w-1/2 hover:translate-y-0.5 transition-all'
+                  value={question.type}
+                  onChange={(e) => updateQuestionType(question.id, e.target.value)}
+                  className='my-auto mx-auto font-bold w-1/2 hover:translate-y-0.5 transition-all'
                 >
-                    <option className='bg-sky-100 w-full' value="MCQ">MCQ</option>
-                    <option className='bg-sky-100 w-full' value="Theory">Theory</option>
+                  <option className='bg-sky-100 w-full' value="MCQ">MCQ</option>
+                  <option className='bg-sky-100 w-full' value="Theory">Theory</option>
                 </select>
-                </label>
+              </label>
             </div>
 
             <div className='my-4 flex justify-between'>
-            {/* Select Difficulty Level */}
-            <label className='my-auto border-2 p-2 rounded-full text-center'>
-              Difficulty Level:
-              <select
-                value={question.difficulty}
-                onChange={(e) => updateQuestionDifficulty(question.id, e.target.value)}
-                className={`my-auto mx-auto font-bold w-1/2 hover:translate-y-0.5 transition-all ${
-                    question.difficulty === 'Easy' ? 'text-green-600' :
+              {/* Select Difficulty Level */}
+              <label className='my-auto border-2 p-2 rounded-full text-center'>
+                Difficulty Level:
+                <select
+                  value={question.difficulty}
+                  onChange={(e) => updateQuestionDifficulty(question.id, e.target.value)}
+                  className={`my-auto mx-auto font-bold w-1/2 hover:translate-y-0.5 transition-all ${question.difficulty === 'Easy' ? 'text-green-600' :
                     question.difficulty === 'Medium' ? 'text-yellow-600' :
-                    'text-red-600'
-                  }`}              >
-                <option className='bg-green-200 w-full text-black' value="Easy">Easy</option>
-                <option className='bg-yellow-200 w-full text-black' value="Medium">Medium</option>
-                <option className='bg-red-200 w-full text-black' value="Hard">Hard</option>
-              </select>
-            </label>
-            {/* Marks Input */}
-            <label className='my-auto border-2 p-2 rounded-full text-center'>
-              Marks:
-              <input
-                type="number"
-                value={question.marks}
-                onChange={(e) => updateQuestionMarks(question.id, e.target.value)}
-                min="0"
-                className='font-bold w-20 p-1'
-              />
-            </label>
+                      'text-red-600'
+                    }`}              >
+                  <option className='bg-green-200 w-full text-black' value="Easy">Easy</option>
+                  <option className='bg-yellow-200 w-full text-black' value="Medium">Medium</option>
+                  <option className='bg-red-200 w-full text-black' value="Hard">Hard</option>
+                </select>
+              </label>
+              {/* Marks Input */}
+              <label className='my-auto border-2 p-2 rounded-full text-center'>
+                Marks:
+                <input
+                  type="number"
+                  value={question.marks}
+                  onChange={(e) => updateQuestionMarks(question.id, e.target.value)}
+                  min="0"
+                  className='font-bold w-20 p-1'
+                />
+              </label>
             </div>
 
             {/* Render appropriate editor based on the question type */}
@@ -172,7 +171,7 @@ function ExamBuilder() {
                       }
                       className='border-2 border-gray-300 my-1 p-2 rounded-md mx-auto w-[90%]'
                     />
-                    
+
                   </div>
                 ))}
               </div>
